28ed82d7c3870b61de54c2ee96ca781d4af0eadd
CIITRFN001 <it@ciit.edu.ph>
2025-03-04 19:33:33 +0800

    firebase

---
+++ practice_routing/src/firebase/config.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAuHETjHXlkWw8_Rky2mzfn0gKDYqcQpPY",
+  authDomain: "article-demo-565f0.firebaseapp.com",
+  projectId: "article-demo-565f0",
+  storageBucket: "article-demo-565f0.firebasestorage.app",
+  messagingSenderId: "25053649113",
+  appId: "1:25053649113:web:03e06af3aa8664050e46c8",
+  measurementId: "G-58GMP56XJY"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
