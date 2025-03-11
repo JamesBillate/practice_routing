5efb44abd99af21ac1875fb8f2434674e5bfbec8
JamesBillate <james.billate@ciit.edu.ph>
2025-03-11 21:38:57 +0800

    Update of Node and Firebase

--- practice_routing/src/firebase/config.js
+++ practice_routing/src/firebase/config.js
@@ -1,6 +1,5 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
 import { getFirestore } from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -14,11 +13,11 @@ const firebaseConfig = {
   storageBucket: "article-demo-565f0.firebasestorage.app",
   messagingSenderId: "25053649113",
   appId: "1:25053649113:web:03e06af3aa8664050e46c8",
-  measurementId: "G-58GMP56XJY"
+  measurementId: "G-58GMP56XJY",
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 
-export {db};
+export { db };
